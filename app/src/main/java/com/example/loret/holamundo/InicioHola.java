0a81f3ee7d7d9aeeff774945b094f41f768a748a
ltrujillog <37044605+ltrujillog@users.noreply.github.com>
2018-03-04 10:52:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/loret/holamundo/InicioHola.java
@@ -0,0 +1,13 @@
+package com.example.loret.holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InicioHola extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_hola);
+    }
+}
